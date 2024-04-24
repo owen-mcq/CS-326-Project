ba87f97c0c9da89adbf3f1faa6a30f22a8d05116
Chandan Marle <chandanmarlex12@gmail.com>
2024-04-24 19:22:21 -0400

    Update html, css and js for homepage to make it look more like wireframe

--- src/docs/milestone-02/script.js
+++ src/docs/milestone-02/script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Get all profiles
     const profiles = document.querySelectorAll("#team li");
-    
+    const dropdownNavBar = document.querySelector(".dropdownNavBar");
+    const links = document.querySelector(".links");
     // Add event listener to each profile
+    dropdownNavBar.addEventListener("click", function(){
+        links.classList.toggle("show");
+    })
     profiles.forEach(profile => {
         profile.addEventListener("click", function() {
             // Find bio section and toggle it to show
