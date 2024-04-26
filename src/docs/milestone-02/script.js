43daf9bfcbc381a8b0ecdd1016f884d08e84827b
Chandan Marle <chandanmarlex12@gmail.com>
2024-04-25 20:40:31 -0400

    Make meet the team page

--- src/docs/milestone-02/script.js
+++ src/docs/milestone-02/script.js
@@ -1,41 +1,50 @@
 import { addStock } from "./db.js";
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get all profiles
-    const profiles = document.querySelectorAll("#team li");
-    const btn = document.querySelector(".dropBtn");
-    const side = document.querySelector(".side");
-    const shadow = document.querySelector(".shadow");
-    const search = document.querySelector(".search [type='text']");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all profiles
+  const profiles = document.querySelectorAll("#team li");
+  const btn = document.querySelector(".dropBtn");
+  const side = document.querySelector(".side");
+  const shadow = document.querySelector(".shadow");
+  const search = document.querySelector(".search [type='text']");
 
-    //Brings out the sidebar when the menu button is clicked
-    btn.addEventListener("click", function(){
-        side.classList.toggle("on");
-        shadow.classList.toggle("on");
+  //Brings out the sidebar when the menu button is clicked
+  if (btn) {
+    btn.addEventListener("click", function () {
+      side.classList.toggle("on");
+      shadow.classList.toggle("on");
     });
+  }
 
-    //When the user clicks on any area that is not the side bar it will close the sidebar
-    shadow.addEventListener("click", function(){
-        side.classList.remove("on");
-        shadow.classList.remove("on");
+  //When the user clicks on any area that is not the side bar it will close the sidebar
+  if (shadow) {
+    shadow.addEventListener("click", function () {
+      side.classList.remove("on");
+      shadow.classList.remove("on");
     });
-
-    profiles.forEach(profile => {
-        profile.addEventListener("click", function() {
-            // Find bio section and toggle it to show
-            const bio = this.querySelector(".bio");
-            bio.classList.toggle("show");
-        });
+  }
+  profiles.forEach((profile) => {
+    profile.addEventListener("click", function () {
+      // Find bio section and toggle it to show
+      const bio = this.querySelector(".bio");
+      bio.classList.toggle("show");
+      if (bio.style.display === "block") {
+        bio.style.display = "none";
+      } else {
+        bio.style.display = "block";
+      }
     });
+  });
 
-    search.addEventListener("keypress", async (event)=>{
-        if (event.key === "Enter") {
-            const stock = search.value.trim();
-            if(stock !== ""){
-                await addStock(stock);
-                search.value = "";
-            }
+  if (search) {
+    search.addEventListener("keypress", async (event) => {
+      if (event.key === "Enter") {
+        const stock = search.value.trim();
+        if (stock !== "") {
+          await addStock(stock);
+          search.value = "";
         }
-    });    
-
-}); 
+      }
+    });
+  }
+});
