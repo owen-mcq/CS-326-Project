972cc742a0380718f9cb65af3e161fec73a59165
owen-mcq <newomcq@gmail.com>
2024-05-10 17:58:00 -0400

    small updates

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ import logger from "morgan";
 const app = express();
 const port = 3000;
 app.use(logger("dev"));
-app.use(express.static("src/client"))
+app.use(express.static("src/client"));
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
   });
